6b99f34060e2e68080e9b44468966ca3568bfd75
andre <oliveir.andre@ua.pt>
2018-05-10 20:34:31 +0100

    added image search

--- ProjetoFinalBD/ProjetoFinalBD/Window1.xaml.cs
+++ ProjetoFinalBD/ProjetoFinalBD/Window1.xaml.cs
@@ -17,11 +17,46 @@ namespace ProjetoFinalBD
     /// <summary>
     /// Lógica interna para Window1.xaml
     /// </summary>
+
     public partial class Window1 : Window
     {
+        private static int i = 0;
+        private static int j = 1;
+        private static int k = 2;
+        private static string[] albums = new string[]   
+        {
+            @"C:\Users\Andre\Desktop\BD-Proj\ProjetoFinalBD\ProjetoFinalBD\1.png",
+            @"C:\Users\Andre\Desktop\BD-Proj\ProjetoFinalBD\ProjetoFinalBD\2.jpg",
+            @"C:\Users\Andre\Desktop\BD-Proj\ProjetoFinalBD\ProjetoFinalBD\3.jpg",
+            @"C:\Users\Andre\Desktop\BD-Proj\ProjetoFinalBD\ProjetoFinalBD\4.jpg",
+            @"C:\Users\Andre\Desktop\BD-Proj\ProjetoFinalBD\ProjetoFinalBD\5.jpg"
+        };
+
         public Window1()
         {
             InitializeComponent();
+            Principal.Source = new BitmapImage(new Uri(albums[j]));
+            Sec1.Source = new BitmapImage(new Uri(albums[k]));
+            Sec2.Source = new BitmapImage(new Uri(albums[i]));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            i++; j++; k++;
+            if (k == albums.Length) k = 0;
+            if (j == albums.Length) j = 0; if (i == albums.Length) i = 0;
+            Principal.Source = new BitmapImage(new Uri(albums[j]));
+            Sec1.Source = new BitmapImage(new Uri(albums[k]));
+            Sec2.Source = new BitmapImage(new Uri(albums[i]));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            i--; j--; k--;
+            if (i < 0) i = albums.Length-1; if (j < 0) j = albums.Length - 1; if (k < 0) k = albums.Length - 1;
+            Principal.Source = new BitmapImage(new Uri(albums[j]));
+            Sec1.Source = new BitmapImage(new Uri(albums[k]));
+            Sec2.Source = new BitmapImage(new Uri(albums[i]));
         }
     }
 }
